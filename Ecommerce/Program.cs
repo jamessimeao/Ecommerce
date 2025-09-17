b4060334acacb9485e939fc456e4018f11a28bd1
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-17 12:52:50 -0300

    Update Program.cs
    Configure the connection with the PostgreSQL database, configure Swagger and map the Identity API endpoints.

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -1,20 +1,45 @@
+#define USE_SWAGGER
+
 using Ecommerce.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#if USE_SWAGGER
+builder.Services.AddSwaggerGen();
+#endif
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if(connectionString == null)
+{
+    Console.WriteLine("Failed to read the connection string.");
+    return;
+}
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Add a connection to the PostgreSQL database without Entity Framework.
+// This will be used for Dapper.
+builder.Services.AddTransient<IDbConnection>(
+    (IServiceProvider serviceProvider) => new NpgsqlConnection(connectionString)
+);
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+#if USE_SWAGGER
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapSwagger();
+#endif
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -34,6 +59,12 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
+#if USE_SWAGGER
+// Not included in the MVC template.
+// Map the Identity Api, so I can at work with the endpoints in swagger.
+app.MapIdentityApi<IdentityUser>();
+#endif
 
 app.MapControllerRoute(
     name: "default",
