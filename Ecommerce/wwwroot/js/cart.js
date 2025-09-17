1776954ca904cfa02d8dc3b3ddaee6fa555f8f98
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-17 12:50:58 -0300

    Make js functions for the buttons that change the shopping cart
    Make functions in js that call actions from the UserCartController to make changes to the cart, such as adding or removing products. The UserCartController returns a view component of the cart to the JavaScript, as HTML text. This HTML is used to change the page, updating the cart, without reloading the page.

---
+++ Ecommerce/wwwroot/js/cart.js
@@ -0,0 +1,81 @@
+﻿async function addToCart(quantity, productId) {
+    const quantityTypeIsCorrect = Number.isInteger(quantity) && quantity >= 1;
+    const productIdTypeIsCorrect = Number.isInteger(productId) && productId >= 1;
+    // Note that the if part won't execute if quantity=0,
+    // which avoids calling the backend.
+    if (quantityTypeIsCorrect && productIdTypeIsCorrect)
+    {
+        
+        const jsonToSend = JSON.stringify({"quantity": quantity,"productId": productId});
+        const request = new Request("/UserCart/Add",
+            {
+                method: "POST",
+                headers:
+                {
+                    "Content-Type": "application/json",
+                },
+                body: jsonToSend,
+            });
+
+        try {
+            const response = await fetch(request);
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            // update the cart in the HTML
+            document.getElementById("cart").innerHTML = await response.text();
+        }
+        catch (error) {
+            console.log("Error:", error);
+        }
+    }
+    else {
+        console.log("The following inputs of addToCart have incorrect types:");
+        if (quantityTypeIsCorrect == false) console.log(`quantity = ${quantity}`);
+        if (productIdTypeIsCorrect == false) console.log(`productId = ${productId}`);
+    }
+}
+
+async function RemoveFromCart(productId) {
+    const productIdTypeIsCorrect = Number.isInteger(productId) && productId >= 1;
+    if (productIdTypeIsCorrect) {
+        const request = new Request(`/UserCart/RemoveSingleProduct/${productId}`,
+            {
+                method: "DELETE",
+            }
+        );
+        try {
+            const response = await fetch(request);
+            if (!response.ok) {
+                throw (new Error(`Response status: ${response.status}`));
+            }
+
+            // update the cart in the HTML
+            document.getElementById("cart").innerHTML = await response.text();
+        }
+        catch (error) {
+            console.log("Error:", error);
+        }
+    }
+}
+
+async function RemoveCart() {
+    const request = new Request("/Usercart/RemoveAllProducts",
+        {
+            method: "DELETE",
+        }
+    );
+    try {
+        const response = await fetch(request);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        // update the cart in the HTML
+        document.getElementById("cart").innerHTML = await response.text();
+    }
+    catch (error) {
+        console.log("Error:", error);
+    }
+}
