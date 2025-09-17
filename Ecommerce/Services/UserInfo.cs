27aea4bf52b15618b094d2e1e18838569a3f97a0
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-17 12:40:00 -0300

    Make a static class to get the user id

---
+++ Ecommerce/Services/UserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace Ecommerce.Services
+{
+    public static class UserInfo
+    {
+        public static string GetUserId(ClaimsPrincipal User)
+        {
+            string userId = null;
+            foreach (ClaimsIdentity? identity in User.Identities)
+            {
+                Claim? claim = identity.FindFirst(ClaimTypes.NameIdentifier);
+                if (claim != null)
+                {
+                    userId = claim.Value;
+                    break;
+                }
+            }
+            return userId;
+        }
+    }
+}
