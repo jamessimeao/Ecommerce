82895bbe541179008c4386e70a1488a50168fb5f
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-17 14:15:43 -0300

    Create a DTO for UserCartController
    Create a DTO CartEntryRequest that stores a quantity and id of a product, and a bool to decide if a checkout button should be created.

---
+++ Ecommerce/DTOs/CartEntryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.DTOs
+{
+    public class CartEntryRequest
+    {
+        public uint Quantity { get; set; }
+        public uint ProductId { get; set; }
+        public bool CreateCheckoutButton { get; set; }
+    }
+}
