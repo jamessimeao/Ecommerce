9d4e0274e1469a3df1d16210fb07a9369a8a669c
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-17 12:42:10 -0300

    Create a view component for the user cart

---
+++ Ecommerce/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(bool createCheckoutButton)
+        {
+            return View(createCheckoutButton);
+        }
+    }
+}
