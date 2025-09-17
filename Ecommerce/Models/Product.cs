b988730908ffcb8212ad7a2e44e2e181cb8b663a
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-17 12:37:48 -0300

    Create models

---
+++ Ecommerce/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Ecommerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public Decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public int SubcategoryId { get; set; }
+        public required string ImagePath { get; set; }
+
+        public override string ToString()
+        {
+            string str = "Product:\n";
+            str += $"Id = {Id}\n";
+            str += $"Name = {Name}\n";
+            str += $"Price = {Price}\n";
+            str += $"CategoryId = {CategoryId}\n";
+            str += $"SubcategoryId = {SubcategoryId}\n";
+            str += $"ImagePath = {ImagePath}\n";
+            return str;
+        }
+    }
+}
---
+++ Ecommerce/Models/Subcategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.Models
+{
+    public class Subcategory
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
