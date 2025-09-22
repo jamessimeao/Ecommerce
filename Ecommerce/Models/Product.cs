3f184ec8dc1d7eb87ff230326f275f0fc3b604b1
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-22 18:41:34 -0300

    Delete CategoryId and SubcategoryId from Product

--- Ecommerce/Models/Product.cs
+++ Ecommerce/Models/Product.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
-
-namespace Ecommerce.Models
+﻿namespace Ecommerce.Models
 {
     public class Product
     {
         public uint Id { get; set; }
         public required string Name { get; set; }
         public Decimal Price { get; set; }
-        public uint CategoryId { get; set; }
-        public uint SubcategoryId { get; set; }
         public required string ImagePath { get; set; }
 
         public override string ToString()
@@ -17,8 +13,6 @@ namespace Ecommerce.Models
             str += $"Id = {Id}\n";
             str += $"Name = {Name}\n";
             str += $"Price = {Price}\n";
-            str += $"CategoryId = {CategoryId}\n";
-            str += $"SubcategoryId = {SubcategoryId}\n";
             str += $"ImagePath = {ImagePath}\n";
             return str;
         }
