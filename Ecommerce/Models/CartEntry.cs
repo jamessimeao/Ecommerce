fedaa74275f88096a0fd5f86726b6b9ba0b53765
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-22 18:37:41 -0300

    Create a new CartEntry model
    The new model stores a Product object instead of its id.

--- Ecommerce/Models/CartEntry.cs
+++ Ecommerce/Models/CartEntry.cs
@@ -1,19 +1,8 @@
 ﻿namespace Ecommerce.Models
 {
-    // Same as Product, but with a Quantity property.
-    // It would be better if an instance of Product was stored as a property,
-    // but that turns the interaction with the database more difficult.
     public class CartEntry
     {
         public uint Quantity { get; set; }
-        public uint ProductId { get; set; }
- 
-        public override string ToString()
-        {
-            string str = "CartEntry:\n";
-            str += $"Quantity = {Quantity}\n";
-            str += $"ProductId = {ProductId}\n";
-            return str;
-        }
+        public required Product Product { get; set; }
     }
 }
