519637784ad3de51514fec61bf5aa65a4dc0122b
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-22 18:36:00 -0300

    Create ClientRequestDTO

---
+++ Ecommerce/DTOs/ClientRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.DTOs
+{
+    public class ClientRequestDTO
+    {
+        public uint Quantity { get; set; }
+        public uint ProductId { get; set; }
+        public bool CreateCheckoutButton { get; set; }
+    }
+}
