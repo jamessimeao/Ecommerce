5b1005590415955c123d5be27d1f2655aa3c067c
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-22 18:36:47 -0300

    Rename the CartEntry model to CartEntryDTO

---
+++ Ecommerce/DTOs/CartEntryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce.DTOs
+{
+    // Same as Product, but with a Quantity property.
+    // It would be better if an instance of Product was stored as a property,
+    // but that turns the interaction with the database more difficult.
+    public class CartEntryDTO
+    {
+        public uint Quantity { get; set; }
+        public uint ProductId { get; set; }
+    }
+}
