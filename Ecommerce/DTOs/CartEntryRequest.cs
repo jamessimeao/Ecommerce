70e7dfee9e2b15c958c23a6f024533e4c17e1bad
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-09-22 18:35:22 -0300

    Rename CartEntryRequest to ClientRequestDTO

--- Ecommerce/DTOs/CartEntryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ecommerce.DTOs
-{
-    public class CartEntryRequest
-    {
-        public uint Quantity { get; set; }
-        public uint ProductId { get; set; }
-        public bool CreateCheckoutButton { get; set; }
-    }
-}
